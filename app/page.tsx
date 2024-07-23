80f83cf587bed8b07ada22e6bb50ec0ba646c8e5
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2024-07-23 21:12:15 +0530

    testing git for now

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello proctise project in progress</h1>
-      <h2>changes are working or not</h2>
     </main>
   );
 }
