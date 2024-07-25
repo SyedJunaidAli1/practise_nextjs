5a3bcbc2461756296024f836850b5f514b3683aa
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2024-07-25 13:49:17 +0530

    nav attached with hero section

---
+++ app/Nav.tsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+        <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+            <div>Gallary</div>
+        </nav>
+    )
+}
+
+
+export default Nav
